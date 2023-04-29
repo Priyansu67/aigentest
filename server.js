d75f1df34587450a3320f713b080812a8b7dd71b
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-04-29 16:18:13 +0530

    Updated 39

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Configuration, OpenAIApi } from "@openai/api";
+import { Configuration, OpenAIApi } from "openai";
 import path from "path";
 import axios from "axios";
 import Replicate from "replicate";
