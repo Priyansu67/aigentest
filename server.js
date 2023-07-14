b41396d7524851d8090937a0d9174dd8922e5c77
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-07-14 20:04:02 +0530

    Changed OpenAI API

--- server.js
+++ server.js
@@ -6,28 +6,101 @@ import axios from "axios";
 import Replicate from "replicate";
 import fetch from "node-fetch";
 import * as dotenv from "dotenv";
+import { get } from "http";
+const app = express();
+app.use(bodyParser.json());
 dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-async function botMessage(prompt) {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-4",
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: prompt },
-    ],
-  });
 
-  return completion.data.choices[0].message;
-}
+const messages = [
+  {
+    role: "system",
+    content: `You are customer service agent of a company named Noise who make smart watches and you are here to provide support on a product called Noisefit Halo Plus. Here are some basic FAQ related to the product Does NoiseFit Halo Plus have a full touch screen display?
+  Yes, NoiseFit Halo Plus features a full touch 1.46” AMOLED display.
+  Is NoiseFit Halo Plus shockproof?
+  No, NoiseFit Halo Plus is not shockproof. Please avoid dropping it from a height as the watch may get damaged.
+  Is NoiseFit Halo Plus waterproof?
+  Yes. NoiseFit Halo Plus has an IP68 rating. It can go underwater up to a depth of 1.0m for up to 30 minutes. However, avoid saunas, hot water and ocean water.
+  Can NoiseFit Halo Plus be used while swimming?
+  You cannot wear it while swimming.
+  Can I take a photo with NoiseFit Halo Plus ?
+  No. NoiseFit Halo Plus does not feature a remote camera. 
+  What is the pixel resolution of my NoiseFit Halo Plus ?
+  The display resolution of NoiseFit Halo Plus is 466*466px.
+  What notifications can I get on my NoiseFit Halo Plus ?
+  You get notifications for SMS, calls, calendar, email, calender, WhatsApp, LinkedIn, Instagram, FB messenger, Skype, Twitter, Facebook, YouTube, Gmail, Outlook, Snapchat and Telegram, as long as the notification feature is turned on and your phone and smartwatch are in Bluetooth range and in sync.
+  Does NoiseFit Halo Plus come with a calling feature?
+  Yes, NoiseFit Halo Plus comes with Bluetooth calling.
+  Does NoiseFit Halo Plus have voice assistance?
+  No, NoiseFit Halo Plus does not come with voice assistance.
+  Does NoiseFit Halo Plus come with in-built games?
+  No, the NoiseFit Halo Plus does not come with in-built games.
+  Does it have a phone tracking option?
+  Yes, the NoiseFit Halo Plus comes with a phone tracking option. 
+  Does NoiseFit Halo Plus have Gesture control?
+  No, NoiseFit Halo Plus does not come with Gesture controls. 
+  Can I reduce the vibration level in my NoiseFit Halo Plus ?
+  Yes, you can control the vibration in NoiseFit Halo Plus .
+  Can NoiseFit Halo Plus store and play music?
+  NoiseFit Halo Plus has a remote music control feature that controls music played on your phone as long as your smartphone and smartwatch are in Bluetooth range. However, it cannot be used to store music.
+  Can I use my earbuds and my NoiseFit Halo Plus at a time with the same smartphone?
+  Yes, both earbuds and NoiseFit Halo Plus can be paired with your smartphone at the same time.
+  What is the Bluetooth range of NoiseFit Halo Plus ?
+  The Bluetooth range of NoiseFit Halo Plus is 10 meters. 
+  Can it play music to a Bluetooth headset?
+  NoiseFit Halo Plus can control the music played through your smartphone as long as it is connected and synced with the app. The NoiseFit Halo Plus can also control music played via Bluetooth headset as long as the smartphone is paired to the Bluetooth headset and the smartwatch.
+  How many watch faces does NoiseFit Halo Plus have?
+  NoiseFit Halo Plus comes with 100+ watch faces which you can access and customise via the NoiseFit app.
+  Can we change the watch face?
+  Yes, you can change the watch faces. From the watch, touch and hold the home screen. Swipe and choose from the watch faces. From the app, go to the watch face, select the watch face of your choice and tap Save to change the watch face. You can even customise your watch face from the app.
+  How do I increase the brightness of my NoiseFit Halo Plus ?
+  You can set the brightness from the watch. Turn on the watch screen, swipe up from the home screen, select Brightness and adjust it as per your convenience.
+  What sensors does the NoiseFit Halo Plus have?
+  NoiseFit Halo Plus features HR sensor, Accelerometer and SpO2 sensor.
+  Does NoiseFit Halo Plus have GPS?
+  No, NoiseFit Halo Plus does not have GPS.
+  What are the accessories included with NoiseFit Halo Plus?
+  NoiseFit Halo Plus comes with one user manual and magnetic charging cable.
+
+  Provide support and never leave the customer hanging and for complex queries, you can always transfer the chat to a human agent.
+  To transfer the chat to a human agent, reply with JSON object containing the following key value pairs:
+    {   
+        "transfer": true
+    }
+  `,
+  },
+  {
+    role: "assistant",
+    content: "How can I help you?",
+  },
+];
+
+const getResponse = async (prompt) => {
+  messages.push({ role: "user", content: prompt });
+  await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+    })
+    .then((response) => {
+      messages.push({
+        role: "assistant",
+        content: response.data.choices[0].message.content,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      return messages;
+    });
+};
 
 //Whatsapp Part
-
-const app = express();
-app.use(bodyParser.json());
 
 const access_token = process.env.ACCESS_TOKEN;
 const myToken = process.env.MY_TOKEN;
@@ -81,7 +154,8 @@ app.post("/webhook", async (req, res) => {
           prompt = prompt.replace("/imagine", "");
           reply = await repliPrompt(prompt);
         } else {
-          reply = await botMessage("Human: " + prompt + "\n\n" + "AI: ");
+          const response = await getResponse(prompt);
+          reply = response[response.length - 1].content;
         }
       } else if (message.image.id) {
         // Handle incoming media message
